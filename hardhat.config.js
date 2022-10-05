3916d129d5a9904e197e42cc2fb5a110dfbbc033
okolicodes7 <okolifavour50@gmail.com>
2022-10-05 21:51:24 +0100

    1st committ

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
+
+// Exchange Contract Address: 0xb90494551526166d0320C1c0e59E56d10A42C602
